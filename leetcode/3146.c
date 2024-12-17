8ca632ed183dfa93d91e618b64a897234f5296f9
Eduardo Pilati <pilatieduardo@gmail.com>
2024-12-17 14:39:54 -0300

    solving 3146

---
+++ leetcode/3146.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int findPermutationDifference(char* s, char* t) {
+    int sl[26];
+    int tl[26];
+    int difference = 0;
+    int len = strlen(s);
+    int idx, i;
+
+
+    for (i = 0; i<len; i++) {
+        sl[(s[i] - 'a')] = i;
+        tl[(t[i] - 'a')] = i;
+    }
+
+    for (i = 0; i<len; i++) {
+        idx = s[i] - 'a';
+        difference += abs(sl[idx] - tl[idx]);
+    }
+    
+    return difference;
+}
+
+int main() {
+    char s[] = "abc";
+    char t[] = "bac";
+    printf("2 == %d\n", findPermutationDifference(s, t));
+
+    char s2[] = "abcde";
+    char t2[] = "edbac";
+    printf("12 == %d\n", findPermutationDifference(s2, t2));
+
+    return 0;
+}
